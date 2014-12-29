55e73f8b95f331fe302a4132b1013bedbb469f8d
Ingvar Khlebnikov <SkaldVolx@mail.ru>
2014-12-29 14:46:09 +0200

    fifth commit

--- db/schema.rb
+++ db/schema.rb
@@ -13,6 +13,9 @@
 
 ActiveRecord::Schema.define(version: 20141218141759) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "answers", force: true do |t|
     t.text     "body"
     t.integer  "user_id"
